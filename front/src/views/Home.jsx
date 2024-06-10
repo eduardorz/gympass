1cad4571cf81951f7f5e6c8cdc8e9f3342e8bb02
eduardorz <ruizzmarin20@gmail.com>
2024-06-10 20:25:34 -0300

    instalacion tailwind

--- front/src/views/Home.jsx
+++ front/src/views/Home.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function Home() {
   return (
     <div>
-        <h1>BIENVENIDO A GYMPASS</h1>
-        <h2>Reserva turnos para venir a entrenar a los mejores gimnasios de la ciudad</h2>
+        <h1 className='text-3xl font-bold font-mono'>BIENVENIDO A GYMPASS</h1>
+        <h2 className='text-3xl font-bold font-serif'>Reserva turnos para venir a entrenar a los mejores gimnasios de la ciudad</h2>
     </div>
   )
 }
--- front/src/views/Login.jsx
+++ front/src/views/Login.jsx
@@ -48,17 +48,28 @@ function LoginUser() {
 
   return (
     <div>
-      <h2>Inicia Sesión</h2>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} class="bg-white dark:bg-gray-900 shadow-md rounded-lg px-8 py-6 max-w-md">
+      <h2 class="text-2xl font-bold text-center mb-4 dark:text-gray-200">Inicia Sesión</h2>
         <div className={styles.container}>
-          <div>
-            <label htmlFor="email">Correo Electrónico:</label>
-            <input type="email" name="email" value={userData.email} onChange={handleChange} required className={styles.input}/>
+          <div class="mb-4">
+            <label htmlFor="email" class="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2" >Correo Electrónico:</label>
+            <input type="email" name="email" class="shadow-sm rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" value={userData.email} onChange={handleChange} required className={styles.input}/>
           </div>
-          <div>
-            <label htmlFor="password">Contraseña:</label>
-            <input type="password" name="password" value={userData.password} onChange={handleChange} required className={styles.input}/>
+          <div class="mb-4">
+            <label htmlFor="password" class="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">Contraseña:</label>
+            <input type="password" name="password" class="shadow-sm rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500" value={userData.password} onChange={handleChange} required className={styles.input}/>
+            <a href="#"
+					class="text-xs text-gray-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Forgot Password?</a>
           </div>
+          <div class="flex items-center justify-between mb-4">
+				<div class="flex items-center">
+					<input type="checkbox" id="remember" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500 focus:outline-none" checked />
+					<label for="remember" class="ml-2 block text-sm text-gray-700 dark:text-gray-300">Remember me</label>
+				</div>
+				<a href="#"
+					class="text-xs text-indigo-500 hover:text-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Create
+					Account</a>
+			</div>
           <div>
             <button type="submit" className={styles.button}>Ingresar</button>
             <button type="submit" className={styles.button} onClick={handleClick}>¿Aún no eres usuario?</button>
